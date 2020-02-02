e1d5ebc0f2c59c74003dfbdb80b20e79a9950345
massaoud <massaoud10@gmail.com>
2020-02-02 17:12:19 +0400

    first commit task 1

--- src/app/routes/task3Router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-
-import {task3s} from '../controllers/task3Controller';
-
-router.get('/task3s',task3s);
-
-module.exports  = router;
